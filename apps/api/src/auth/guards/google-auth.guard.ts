2c0d650304f71692fabfdf12e464899a4dc44219
xwaynex <waynexaint@gmail.com>
2026-07-02 23:15:59 +0100

    feat(auth): enhance GoogleAuthGuard to support offline access type for refresh token

--- apps/api/src/auth/guards/google-auth.guard.ts
+++ apps/api/src/auth/guards/google-auth.guard.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class GoogleAuthGuard extends AuthGuard('google') {}
+export class GoogleAuthGuard extends AuthGuard('google') {
+  // so google can return refresh token, we need to set accessType to offline
+  constructor(private configService: ConfigService) {
+    super({
+      accessType: 'offline',
+    });
+  }
+}
