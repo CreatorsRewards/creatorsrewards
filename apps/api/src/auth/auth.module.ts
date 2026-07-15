e9bc4a4f6055ea93b88b0c6700bc3c677f05e8c0
xwaynex <waynexaint@gmail.com>
2026-07-15 22:34:33 +0100

    feat(auth): restructure AuthModule to include OAuthService and GoogleProvider

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { AuthService } from './services/auth.service';
 import { AuthController } from './auth.controller';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { GoogleAuthGuard } from './guards/google-auth.guard';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma.module';
+import { OAuthService } from './services/oauth.service';
+import { GoogleProvider } from './providers/google.provider';
 
 @Module({
-  imports: [PassportModule, JwtModule.register({})],
+  imports: [PassportModule, PrismaModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, GoogleStrategy, GoogleAuthGuard],
+  providers: [
+    AuthService,
+    GoogleStrategy,
+    GoogleAuthGuard,
+    OAuthService,
+    GoogleProvider,
+  ],
+  exports: [OAuthService],
 })
 export class AuthModule {}
