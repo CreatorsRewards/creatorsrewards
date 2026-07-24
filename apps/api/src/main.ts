c7b0eadee0cddffa0082567853c17ca36ca13310
xwaynex <waynexaint@gmail.com>
2026-07-24 22:23:36 +0100

    feat(api): set global prefix for all controllers to '/api'

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); // { abortOnError: false }
+
+  // This automatically prepends /api to EVERY controller in your app
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
